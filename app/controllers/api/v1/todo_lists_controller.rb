d80b086bf7f6bdd917646cf7b6f56eb7a0df3d8c
byelipk <byelipk@gmail.com>
2017-06-26 14:26:54 -0400

    Side load todos

--- app/controllers/api/v1/todo_lists_controller.rb
+++ app/controllers/api/v1/todo_lists_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::TodoListsController < ApplicationController
   def index
-    render json: TodoList.all, include: ['todos'], status: 200
+    render json: todo_lists, include: [ "todos" ], status: 200
+  end
+
+  private
+
+  def todo_lists
+    TodoList.includes(:todos).all
   end
 end
