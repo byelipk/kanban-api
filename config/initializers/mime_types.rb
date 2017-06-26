c39d480957cdb3719edf7dac3420c05bab610930
byelipk <byelipk@gmail.com>
2017-06-26 14:55:06 -0400

    Upgrade mime type

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,13 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+# To accept JSON API data with need to upgrade the json mime type
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+Mime::Type.unregister :json
+Mime::Type.register 'application/json', :json, api_mime_types
